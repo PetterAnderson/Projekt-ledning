426e49216a616120e44b18a9bfc9da2c966e437b
danieltomic <45372399+danieltomic@users.noreply.github.com>
2019-03-24 12:06:48 +0100

    Lade in sortByDansExpertis

--- src/modell/KundRegister.java
+++ src/modell/KundRegister.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 
 
 public class KundRegister {
+    private String allDancers;
 
     private ArrayList<Kund> kundLista = new ArrayList <>();
 
     public void addKund(Kund nyKund){
-     kundLista.add(nyKund);
+        kundLista.add(nyKund);
     }
 
     public Kund findKund(String pNr){ //loopar igenom listan, returnerar om matchar, annars returnera null
@@ -21,5 +22,13 @@ public class KundRegister {
 
     public void removeKund(String pNr){
         kundLista.remove(findKund(pNr));
+    }
+
+    public String sortByDansExpertis(int dansExpertis){  //går igenom kundregistret och returnerar emailen av de som har samma dansexpertis som invalt
+        for (int i = 0; i < kundLista.size(); i++){  // kunde inte få det att fungera med en for each loop så fick bli såhär
+            if (kundLista.get(i).getDansExpertis() == dansExpertis){
+                allDancers += kundLista.get(i).getEmail() + "\n";
+            }
+        }return allDancers;  //första emailen den returnerar när används genom controller har "null" framför, så "nulldaniel@gmail.com". idk why
     }
 }
