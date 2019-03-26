17a92694ffd6687e8578c9c78a7e5de6720cf751
nordgust <nordgust@gmail.com>
2019-03-26 12:15:59 +0100

    Update view.java

--- src/view/view.java
+++ src/view/view.java
@@ -20,6 +20,7 @@ import javax.swing.border.MatteBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+
 import javax.swing.JSlider;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
@@ -40,6 +41,10 @@ import java.awt.Cursor;
 import javax.swing.border.LineBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
 import javax.swing.SwingConstants;
 
 public class view {
@@ -288,12 +293,18 @@ public class view {
 			public void actionPerformed(ActionEvent arg0) {
 				DefaultTableModel tableModel = (DefaultTableModel) table_1.getModel();
 				tableModel.setRowCount(0);
+				String emailClip = "";
 				ArrayList <Kund> kund = controller.sortByDansExpertis(Integer.parseInt(comboBox.getSelectedItem().toString()));
 				for(int i = 0; i < kund.size(); i++) {
 					String [] row = {kund.get(i).getNamn(),kund.get(i).getEmail()};
 					tableModel.addRow(row);
+					emailClip +=  tableModel.getValueAt(i, 1) + " ";
 				}
-
+				
+				StringSelection stringSelection = new StringSelection (emailClip);
+				Clipboard clpbrd = Toolkit.getDefaultToolkit ().getSystemClipboard ();
+				clpbrd.setContents (stringSelection, null);
+				
 			}
 		});
 		
